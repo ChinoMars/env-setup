4987641ba0b280ad6f8f43aa02d0995424bcf9ca
chuanjiang.ma <chuanjiang.ma@mthreads.com>
2023-10-12 14:25:59 +0800

    [fix] fix the script for installing
    Signed-off-by: chuanjiang.ma <chuanjiang.ma@mthreads.com>

--- config.sh
+++ config.sh
@@ -6,15 +6,19 @@ HERE=`pwd`
 curl -fkLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if $? -ne 0; then
+if [[ $? -ne 0 ]]; then
     echo "error: download vim-plug failed."
     exit 1
 fi
 
 # config vimrc
 VIMRC_DIR=${HERE}/vim/vimrc
-ln -s ${VRIMRC_DIR} ~/.vimrc
+ln -s ${VIMRC_DIR} ~/.vimrc
 
 # config lldb
 LLDBINIT_DIR=${HERE}/lldb/lldbinit
 ln -s ${LLDBINIT_DIR} ~/.lldbinit
+
+# config tmux
+TMUX_CFG=${HERE}/tmux/tmux.conf
+ln -s ${TMUX_CFG} ~/.tmux.conf
